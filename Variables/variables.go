246e368b36a6a8b9cccb620c66c23fec2296a834
Kasun3865 <ict21945@fot.sjp.ac.lk>
2025-01-02 21:31:09 +0530

    main.io deleted and Add Variables file and execute variables.go file

---
+++ Variables/variables.go
@@ -0,0 +1,15 @@
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println("Kasun Sanjeewa")
+	fmt.Println("Kasun Sanjeewa")
+	fmt.Println("Kasun Sanjeewa")
+	fmt.Println("Kasun Sanjeewa")
+
+	fmt.Print("Kasun Sanjeewa ")
+	fmt.Print("Kasun Sanjeewa ")
+	fmt.Print("Kasun Sanjeewa ")
+	fmt.Print("Kasun Sanjeewa ")
+}
