f7cffdbae96e54b7efcb231d031b753e9b86495a
Kasun3865 <ict21945@fot.sjp.ac.lk>
2025-01-02 22:05:03 +0530

    Variables is ok.

--- Variables/variables.go
+++ Variables/variables.go
@@ -1,15 +1,55 @@
+// package main
+
+// import "fmt"
+
+// func main() {
+// 	fmt.Println("Kasun Sanjeewa")
+// 	fmt.Println("Kasun Sanjeewa")
+// 	fmt.Println("Kasun Sanjeewa")
+// 	fmt.Println("Kasun Sanjeewa")
+
+// 	fmt.Print("Kasun Sanjeewa ")
+// 	fmt.Print("Kasun Sanjeewa ")
+// 	fmt.Print("Kasun Sanjeewa ")
+// 	fmt.Print("Kasun Sanjeewa ")
+// }
+
+// package main
+
+// import (
+// 	"fmt"
+// )
+
+// func main() {
+// 	var firstNumber int = 30
+// 	var secondNumber int = 50
+
+// 	fmt.Println(firstNumber + secondNumber)
+
+// 	var firstName string = "Kasun"
+// 	var lastName string = "Sanjeewa"
+// 	fmt.Println(firstName + lastName)
+
+// 	var floatNumber float32 = 0.15
+// 	var floatNumber2 float64 = 0.1245355
+
+// 	fmt.Println(floatNumber)
+// 	fmt.Println(floatNumber2)
+
+// 	var boolean bool = true
+// 	var boolean2 bool = false
+
+// 	fmt.Println(boolean)
+// 	fmt.Println(boolean2)
+
+// }
+
 package main
 
-import "fmt"
+import (
+	"fmt"
+)
 
 func main() {
-	fmt.Println("Kasun Sanjeewa")
-	fmt.Println("Kasun Sanjeewa")
-	fmt.Println("Kasun Sanjeewa")
-	fmt.Println("Kasun Sanjeewa")
 
-	fmt.Print("Kasun Sanjeewa ")
-	fmt.Print("Kasun Sanjeewa ")
-	fmt.Print("Kasun Sanjeewa ")
-	fmt.Print("Kasun Sanjeewa ")
 }
